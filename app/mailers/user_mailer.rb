8a8c437ae00d76c496dd9d370beac8ad7b0e5f4e
nguyen <nguyenht.nta@gmail.com>
2026-06-02 08:27:32 +0700

    [Add]: jp developer full code 7 01/06/2026

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
-  default from: ENV.fetch("EMAIL_USERNAME", "nguyenht.nta@gmail.com")
-
+  default from: "Daily Nihongo <nguyenht.nta@gmail.com>"
+  
   def welcome_email(user)
     @user = user
 
